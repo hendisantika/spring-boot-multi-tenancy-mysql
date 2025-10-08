69801f53e10b8fb88c9a565604178ed65744d869
hendisantika <hendisantika@yahoo.co.id>
2025-10-09 06:41:40 +0700

    feat: Add LoginRequest🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/multitenancymysql/dto/LoginRequest.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.multitenancymysql.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-tenancy-mysql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/10/25
+ * Time: 05.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
