56f852c550e1365adfb6693cb5eacd2170988426
hendisantika <hendisantika@yahoo.co.id>
2025-10-09 05:54:10 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multitenancymysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiTenancyMysqlApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiTenancyMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multitenancymysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiTenancyMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
