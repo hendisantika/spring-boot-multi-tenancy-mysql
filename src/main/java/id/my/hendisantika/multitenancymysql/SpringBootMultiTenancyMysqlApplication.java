34082ffeb60b7208f41fe08e3b64c137db5f7e3b
hendisantika <hendisantika@yahoo.co.id>
2025-10-09 06:06:32 +0700

    feat: Add required configs🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplication.java
+++ src/main/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplication.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.multitenancymysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-tenancy-mysql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/10/25
+ * Time: 05.14
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootApplication
 public class SpringBootMultiTenancyMysqlApplication {
 
--- src/test/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplicationTests.java
+++ src/test/java/id/my/hendisantika/multitenancymysql/SpringBootMultiTenancyMysqlApplicationTests.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.multitenancymysql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-tenancy-mysql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/10/25
+ * Time: 05.25
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootTest
 class SpringBootMultiTenancyMysqlApplicationTests {
 
